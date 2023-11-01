631023a7e68e5f28bd0583e5c6b20941247a5856
Ludovic Tuccio <ludovic.tuccio@hotmail.fr>
2023-11-01 19:32:01 +0100

    Ajout de la premiere methode d'incrémentation du compteur likes

--- src/app/snap-face/snap-face.component.ts
+++ src/app/snap-face/snap-face.component.ts
@@ -19,4 +19,8 @@ export class SnapFaceComponent implements OnInit {
     this.snaps=6;
     this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
   }
+
+  onAddLike(){
+    this.snaps++;
+  }
 }
