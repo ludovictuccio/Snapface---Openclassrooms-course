3a956a80bb94973cfa32809f0d2373166effaf53
Ludovic Tuccio <ludovic.tuccio@hotmail.fr>
2023-11-09 18:28:43 +0100

    Ajout de tableau pour afficher les éléments avec le ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,40 +8,41 @@ import { SnapFace } from './models/snap-face.model';
 })
 export class AppComponent implements OnInit{
  
-  mySnap!: SnapFace;
-  mySnapTwo!: SnapFace;
-  mySnapThree!: SnapFace;
-
+  snapFaces!: SnapFace[];
+  
   ngOnInit() {
 
-    this.mySnap = {
-      title: 'Archibald',
-      description: 'Mon meilleur ami depuis tout petit !',
-      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      createdDate: new Date(),
-      snaps: 0,
-      isAlreadySnapped:false,
-      statusSnap: 'I like !',
-      location:'La montagne'
-    };
-    this.mySnapTwo = {
-      title: 'Three Rock Mountain',
-      description: 'Un endroit magnifique pour les randonnées.',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
-      createdDate: new Date(),
-      snaps: 0,
-      isAlreadySnapped:false,
-      statusSnap: 'I like !'
-    };
-    this.mySnapThree = {
-      title: 'Un bon repas',
-      description: 'Mmmh que c\'est bon !',
-      imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-      createdDate: new Date(),
-      snaps: 0,
-      isAlreadySnapped:false,
-      statusSnap: 'I like !'
-    };
-
+    this.snapFaces = [
+   {
+        title: 'Archibald',
+        description: 'Mon meilleur ami depuis tout petit !',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+        isAlreadySnapped:false,
+        statusSnap: 'I like !',
+        location:'La montagne'
+      },
+      {
+        title: 'Three Rock Mountain',
+        description: 'Un endroit magnifique pour les randonnées.',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+        isAlreadySnapped:false,
+        statusSnap: 'I like !'
+      },
+       {
+        title: 'Un bon repas',
+        description: 'Mmmh que c\'est bon !',
+        imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+        isAlreadySnapped:false,
+        statusSnap: 'I like !'
+      }
+  
+    ];
+    
   }
 }
