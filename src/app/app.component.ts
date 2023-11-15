54d618d89b137a109dfa231d5ec959ae147160cb
Ludovic Tuccio <ludovic.tuccio@hotmail.fr>
2023-11-15 19:53:25 +0100

    Re-structuration : ajout nouveau service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { SnapFace } from './models/snap-face.model';
 
 @Component({
   selector: 'app-root',
@@ -8,41 +7,7 @@ import { SnapFace } from './models/snap-face.model';
 })
 export class AppComponent implements OnInit{
  
-  snapFaces!: SnapFace[];
-  
-  ngOnInit() {
-
-    this.snapFaces = [
-   {
-        title: 'Archibald',
-        description: 'Mon meilleur ami depuis tout petit !',
-        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-        createdDate: new Date(),
-        snaps: 0,
-        isAlreadySnapped:false,
-        statusSnap: 'I like !',
-        location:'La montagne'
-      },
-      {
-        title: 'Three Rock Mountain',
-        description: 'Un endroit magnifique pour les randonnées.',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
-        createdDate: new Date(),
-        snaps: 0,
-        isAlreadySnapped:false,
-        statusSnap: 'I like !'
-      },
-       {
-        title: 'Un bon repas',
-        description: 'Mmmh que c\'est bon !',
-        imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-        createdDate: new Date(),
-        snaps: 0,
-        isAlreadySnapped:false,
-        statusSnap: 'I like !'
-      }
-  
-    ];
+  ngOnInit(): void {
     
   }
 }
--- src/app/snap-face-list/snap-face-list.component.ts
+++ src/app/snap-face-list/snap-face-list.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { SnapFace } from '../models/snap-face.model';
 
 @Component({
   selector: 'app-snap-face-list',
   templateUrl: './snap-face-list.component.html',
   styleUrls: ['./snap-face-list.component.css']
 })
-export class SnapFaceListComponent {
+export class SnapFaceListComponent implements OnInit {
 
+  snapFaces!: SnapFace[];
+  
+  constructor(){}
+  
+  ngOnInit() {
+
+    this.snapFaces = [
+   {
+        title: 'Archibald',
+        description: 'Mon meilleur ami depuis tout petit !',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+        isAlreadySnapped:false,
+        statusSnap: 'I like !',
+        location:'La montagne'
+      },
+      {
+        title: 'Three Rock Mountain',
+        description: 'Un endroit magnifique pour les randonnées.',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+        isAlreadySnapped:false,
+        statusSnap: 'I like !'
+      },
+       {
+        title: 'Un bon repas',
+        description: 'Mmmh que c\'est bon !',
+        imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+        isAlreadySnapped:false,
+        statusSnap: 'I like !'
+      }
+  
+    ];
+  
+  }
 }
