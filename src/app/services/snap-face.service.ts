bf8ae32bdb59af414e4ba693c16f6dd0e7eec4d2
Ludovic Tuccio <ludovic.tuccio@hotmail.fr>
2023-11-17 19:52:12 +0100

    Ajout d'un service
    Les services permettent de centraliser les données et la logique pour les différents domaines de votre application.
    
    Créer un service est aussi simple qu'ajouter le décorateur  @Injectable()  à une classe.
    Pour injecter un service dans un component, ajoutez un argument au constructor du component qui a le type du service, par exemple private userService: UserService

---
+++ src/app/services/snap-face.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { SnapFace } from '../models/snap-face.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SnapFaceService {
+  
+    snapFaces: SnapFace[] = [
+
+        {
+            title: 'Archibald',
+            description: 'Mon meilleur ami depuis tout petit !',
+            imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+            createdDate: new Date(),
+            snaps: 0,
+            isAlreadySnapped:false,
+            statusSnap: 'I like !',
+            location:'La montagne'
+          },
+          {
+            title: 'Three Rock Mountain',
+            description: 'Un endroit magnifique pour les randonnées.',
+            imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+            createdDate: new Date(),
+            snaps: 0,
+            isAlreadySnapped:false,
+            statusSnap: 'I like !'
+          },
+           {
+            title: 'Un bon repas',
+            description: 'Mmmh que c\'est bon !',
+            imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+            createdDate: new Date(),
+            snaps: 0,
+            isAlreadySnapped:false,
+            statusSnap: 'I like !'
+          }
+      
+        ];
+      
+        
+    
+
+}
--- src/app/snap-face-list/snap-face-list.component.ts
+++ src/app/snap-face-list/snap-face-list.component.ts
@@ -1,5 +1,6 @@
 import { Component , OnInit} from '@angular/core';
 import { SnapFace } from '../models/snap-face.model';
+import { SnapFaceService } from '../services/snap-face.service';
 
 @Component({
   selector: 'app-snap-face-list',
@@ -10,41 +11,11 @@ export class SnapFaceListComponent implements OnInit {
 
   snapFaces!: SnapFace[];
   
-  constructor(){}
+  constructor(private snapFaceService : SnapFaceService){}
   
   ngOnInit() {
 
-    this.snapFaces = [
-   {
-        title: 'Archibald',
-        description: 'Mon meilleur ami depuis tout petit !',
-        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-        createdDate: new Date(),
-        snaps: 0,
-        isAlreadySnapped:false,
-        statusSnap: 'I like !',
-        location:'La montagne'
-      },
-      {
-        title: 'Three Rock Mountain',
-        description: 'Un endroit magnifique pour les randonnées.',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
-        createdDate: new Date(),
-        snaps: 0,
-        isAlreadySnapped:false,
-        statusSnap: 'I like !'
-      },
-       {
-        title: 'Un bon repas',
-        description: 'Mmmh que c\'est bon !',
-        imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-        createdDate: new Date(),
-        snaps: 0,
-        isAlreadySnapped:false,
-        statusSnap: 'I like !'
-      }
-  
-    ];
+    this.snapFaces = this.snapFaceService.snapFaces;
   
   }
 }
